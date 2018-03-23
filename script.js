e7a28cec13ce35b92f1f134497ace4a82db78061
netmag <netmag@list.ru>
2018-03-23 13:22:45 +0300

    скрипт для модальных окон

---
+++ script.js
@@ -0,0 +1,27 @@
+window.onload = main;
+
+'use strict'
+
+function main() {
+	const qS = selector => document.querySelector(selector);
+	const modalCloseMap = qS(".modal-close-map");
+	const modalClose = qS(".modal-close");
+	const modalMap = qS(".modal-map");
+	const modalLogin = qS(".modal-login");
+	const map = qS(".map");
+	const loginLink = qS(".login-link");
+
+	const visible = elem =>	elem.style.visibility = 'visible';
+	const hidden = elem => elem.style.visibility = 'hidden';
+
+	loginLink.onmouseup = () => visible(modalLogin);
+	map.onmouseup = () => visible(modalMap);
+	modalClose.onmouseup = () => hidden(modalLogin);
+	modalCloseMap.onmouseup = () => hidden(modalMap);
+	// document.getElementById('loglin').onclick = () => {
+	// 	this.style.visibility = 'visible';
+	// };
+
+
+
+};
